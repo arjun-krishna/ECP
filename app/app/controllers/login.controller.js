d0977f06519957f967d2073115b62cec384134f5
suhaschintala <suhaschintala@gmail.com>
2017-02-25 15:51:30 +0530

    stupid bug

--- app/app/controllers/login.controller.js
+++ app/app/controllers/login.controller.js
@@ -12,7 +12,7 @@ angular.module('App')
     	// $location.path('/user/'+$scope.username);
     	$http({
     		'method'	: 'POST',
-    		'url'		: 'htpp://127.0.0.1:3000/citizen/login/',
+    		'url'		: 'http://127.0.0.1:3000/citizen/login/',
     		'data'		: {'username' : $scope.username, 'password': $scope.password}			
     	})
     	.then(function (resp) {
