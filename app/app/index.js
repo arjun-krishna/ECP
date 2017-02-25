0fb73f67f0ab9699bfa4596824748f12beee8b74
suhaschintala <suhaschintala@gmail.com>
2017-02-25 12:32:55 +0530

    added basic structure for app clientside

---
+++ app/app/index.js
@@ -0,0 +1,11 @@
+var app = angular.module('App', [
+  'ngRoute'
+]);
+app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+});
